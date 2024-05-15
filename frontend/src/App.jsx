2ae412f4f5a4db80b2142b50ca7845b5d4e8e455
Ahmed-Egal <egalosman101@gmail.com>
2024-05-15 15:47:54 +0300

    Morning supplications rendered, counter added
    The morning supplications have been added and rendered. The counter is added and styled for this section.
    [NEXT] - Increment and decrement features for the counter buttons, theme change on hover of counter, styling the two sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import reset_icon from './assets/new-bx-reset.svg'
+// import morningSupplications from './duas'
+import Duas from './Duas/'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,10 @@ function App() {
   }
   const resetCounter = () => {
     setCount(0)
+  }
+
+  const fetchSupplications = () => {
+
   }
 
   return (
@@ -24,7 +30,7 @@ He knows what happens to them in this world, and what will happen to them in the
 And they will never encompass anything of His Knowledge except that which He wills.
 His Throne extends over the heavens and the earth,
 and He feels no fatigue in guarding and preserving them.
-And He is the Most High, the Most Great.</p>
+And He is the Most High, the Most Great.<br></br><br></br><small className='times-to-read'>Read 3 times</small></p>
         
        <p className='arabic-text'>
         اللّهُ لاَ إِلَـهَ إِلاَّ هُوَ الْحَيُّ الْقَيُّومُ لاَ تَأْخُذُهُ سِنَةٌ وَلاَ نَوْمٌ لَّهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الأَرْضِ مَن ذَا الَّذِي يَشْفَعُ عِنْدَهُ إِلاَّ بِإِذْنِهِ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ وَلاَ يُحِيطُونَ بِشَيْءٍ مِّنْ عِلْمِهِ إِلاَّ بِمَا شَاء وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالأَرْضَ وَلاَ يَؤُودُهُ حِفْظُهُمَا وَهُوَ الْعَلِيُّ الْعَظِيمُ. 
@@ -39,6 +45,109 @@ And He is the Most High, the Most Great.</p>
        
        </div>
       </div>
+      <br></br>
+      <br></br>
+
+      <div className="supplication-cards">
+      <p className='english-text'>In the name of Allah,
+the Gracious, the Merciful
+1. Say, “He is Allah, the One.
+2. Allah, the Absolute.
+3. He begets not, nor was He begotten.
+4. And there is nothing comparable to Him.”<br></br><br></br><small className='times-to-read'>Read 3 times</small>
+<br></br>
+Whoever reads this in the morning and evening, it is sufficient for him for every matter.</p>
+        
+       <p className='arabic-text'>
+       بِسْمِ اللهِ الرَّحْمنِ الرَّحِيم
+<br></br>
+قُلْ هُوَ ٱللَّهُ أَحَدٌ<br></br> ٱللَّهُ ٱلصَّمَدُ<br></br> لَمْ يَلِدْ وَلَمْ يُولَدْ<br></br> وَلَمْ يَكُن لَّهُۥ كُفُوًا أَحَدٌۢ
+     
+       </p>
+       <div className='counter-div'>
+
+       <button  onClick={addCounter} className='counter-btn'>{count}</button>
+       <p>hi</p>
+       
+       <img src={reset_icon} onClick={resetCounter}/>
+       
+       </div>
+      </div               >
+
+
+      <br></br>
+      <br></br>
+
+      <div className="supplication-cards">
+      <p className='english-text'>In the name of Allah,
+the Gracious, the Merciful
+1. Say, “He is Allah, the One.
+2. Allah, the Absolute.
+3. He begets not, nor was He begotten.
+4. And there is nothing comparable to Him.”<br></br><br></br><small className='times-to-read'>Read 3 times</small>
+</p>
+        
+       <p className='arabic-text'>
+       بِسْمِ اللهِ الرَّحْمنِ الرَّحِيم
+       <br></br>
+قُلْ أَعُوذُ بِرَبِّ ٱلْفَلَقِ
+<br></br> مِن شَرِّ مَا خَلَقَ<br></br> وَمِن شَرِّ غَاسِقٍ إِذَا وَقَبَ<br></br> وَمِن شَرِّ ٱلنَّفَّٰثَٰتِ فِى ٱلْعُقَدِ<br></br> وَمِن شَرِّ حَاسِدٍ إِذَا حَسَدَ
+     
+       </p>
+       <div className='counter-div'>
+
+       <button  onClick={addCounter} className='counter-btn'>{count}</button>
+       <p>hi</p>
+       {/* <button className='reset-btn'><i class='bx bx-reset bx-flip-horizontal bx-tada' ></i></button> */}
+       <img src={reset_icon} onClick={resetCounter}/>
+       
+       </div>
+      </div               >
+
+      <br></br>
+      <br></br>
+
+      <div className="supplication-cards">
+      <p className='english-text'>In the name of Allah,
+the Gracious, the Merciful
+1. Say, “He is Allah, the One.
+2. Allah, the Absolute.
+3. He begets not, nor was He begotten.
+4. And there is nothing comparable to Him.”<br></br><br></br><small className='times-to-read'>Read 3 times</small>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+Whoever reads this in the morning and evening, it is sufficient for him for every matter.
+<br></br>
+(Includes the three muwaa'idatain and ayatul kursi)</p>
+        
+       <p className='arabic-text'>
+       بِسْمِ اللهِ الرَّحْمنِ الرَّحِيم
+       <br></br>
+قُلْ أَعُوذُ بِرَبِّ ٱلنَّاسِ
+<br></br> مَلِكِ ٱلنَّاسِ ،
+ إِلَٰهِ ٱلنَّاسِ
+<br></br> مِن شَرِّ ٱلْوَسْوَاسِ ٱلْخَنَّاسِ
+<br></br> ٱلَّذِى يُوَسْوِسُ فِى صُدُورِ ٱلنَّاسِ
+<br></br> مِنَ ٱلْجِنَّةِ وَٱلنَّاسِ 
+<br></br><br></br>
+<small className='times-to-read'>
+من قالها حين يصبح وحين يمسى كفته من كل شىء 
+</small>  
+       </p>
+       <div className='counter-div'>
+
+       <button  onClick={addCounter} className='counter-btn'>{count}</button>
+       <p>hi</p>
+       
+       <img src={reset_icon} onClick={resetCounter}/>
+       
+       </div>
+      </div               >
+      <section>
+        <div>
+          <Duas />
+        </div>
+      </section>
+      
     </div>
     </>
   )
